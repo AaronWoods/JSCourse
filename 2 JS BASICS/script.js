b076991fb773adbe3c4474bdf28ebd3d072aeb9f
AaronWoods <aaron.woods22@mail.dcu.ie>
2018-09-17 18:29:07 +0100

    First Challenge complete
    Didn't use if statements as it wasn't covered yet

--- 2 JS BASICS/script.js
+++ 2 JS BASICS/script.js
@@ -1,4 +1,18 @@
-console.log("Hello World!");
+/*************CODING CHALLENGE 1********************/
 
-var firstName = 'Aaron';
-var lastName = 'Woods';
+var mark = {
+    weight : prompt("What is Marks height?"),
+    height : prompt("What is Marks Weight?")
+}
+var john = {
+    weight : prompt("What is Johns height?"),
+    height : prompt("What is Johns Weight?")
+}
+
+var markBMI = mark.weight / (mark.height*mark.height);
+var johnBMI = john.weight / (john.height*john.height);
+
+var higher = markBMI > johnBMI;
+
+console.log("Is Marks BMI higher than Johns?"+ higher);
+console.log(typeof higher); // don't use typeof a lot so added here
