fc30cea3bda41407895ca075a06b47da0a6148dc
AaronWoods <aaron.woods22@mail.dcu.ie>
2018-09-25 16:16:53 +0100

    End of Section 3

--- 3 How JS Works/script.js
+++ 3 How JS Works/script.js
@@ -1,20 +1,35 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+/** FUNCTIONS */
+/*calcAge(1990);
 
+//retirement(1990); THIS FAILS
 
+function calcAge(year){ //function declaration
+    console.log(2016 - year);
+}
 
+var retirement = function(year){ //function expression
+    console.log(65 - (2016 - year));
+}
 
+retirement(1990);
+calcAge(1990);
 
+/** VARIABLES */
+/*console.log(age); //appears as undefined - Hoisting global declarations of functions and variables
+var age = 23;
+console.log(age);
 
+function foo(){ //FOO EXECUTION CONTEXT
+    var age = 65;
+    console.log(age);
+}
+foo(); 
+console.log(age); //GLOBAL EXECUTION CONTEXT
 
-
-
-
-
-
-
-
+*/
 
 
 ///////////////////////////////////////
@@ -63,9 +78,37 @@ function third() {
 // Lecture: The this keyword
 
 
+//console.log(this);
+/*calcAge(1985);
 
+function calcAge(year){
+    console.log(2016 - year);
+    console.log(this);
+}
+*/
 
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calcAge: function(){
+        console.log(this);
+        console.log(2016 - this.yearOfBirth);
+        /*
+        function innerFunction(){
+            console.log(this); //WINDOW OBJECT(GLOBAL)
+        }
+        innerFunction();*/
+    }
+};
 
+john.calcAge();
 
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+};
 
+mike.calcAge = john.calcAge;
+
+mike.calcAge();
 
