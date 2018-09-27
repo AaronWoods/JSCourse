4a7a58b4d58a56ef97a881b54efeb7e74ad9593f
AaronWoods <aaron.woods22@mail.dcu.ie>
2018-09-27 14:46:50 +0100

    Finished Dice game

--- 4 JS in the Browser/app.js
+++ 4 JS in the Browser/app.js
@@ -7,4 +7,86 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
+//document.querySelector('#current-'+ activePlayer).textContent = dice; //CONTEXT
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'; //DOM ELEMENT CHANGE
+
+/*
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+//CALLBACK FUNCTION
+document.querySelector('.btn-roll').addEventListener('click', btn);
+//ANON FUNCTION
+document.querySelector('.btn-roll').addEventListener('click', function(){
 */
+
+var scores, roundScore, activePlayer;
+
+var scores = [0,0];
+var roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    var currentScoreDOM = document.querySelector('#current-'+ activePlayer);
+    var diceDOM = document.querySelector('.dice');
+    var dice = Math.floor(Math.random() * 6) + 1;
+    
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    if(dice !== 1){
+        currentScoreDOM.textContent = parseInt(currentScoreDOM.textContent) + dice;
+    }
+    else{
+        currentScoreDOM.textContent = 0;
+        switchPlayers();
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    var playerScoreDOM = document.querySelector('#score-' + activePlayer);
+    var currentScoreDOM = document.querySelector('#current-' + activePlayer);
+    
+    playerScoreDOM.textContent = parseInt(currentScoreDOM.textContent) + parseInt(playerScoreDOM.textContent);
+    currentScoreDOM.textContent = 0;
+    if(parseInt(playerScoreDOM.textContent) > 99){
+        document.querySelector('.btn-roll').style.display = 'none';
+    }
+    switchPlayers();
+});
+
+document.querySelector('.btn-new').addEventListener('click', function(){
+    console.log("hit");
+    document.querySelector('.dice').style.display = 'none';
+    var playerScoreDOM = document.querySelector('#score-0');
+    var currentScoreDOM = document.querySelector('#current-0');
+    var playerScoreDOM2 = document.querySelector('#score-1');
+    var currentScoreDOM2 = document.querySelector('#current-1');
+    var playerActive = document.querySelector('.player-0-panel');
+    playerScoreDOM.textContent = 0;
+    playerScoreDOM2.textContent = 0;
+    currentScoreDOM.textContent = 0;
+    currentScoreDOM2.textContent = 0;
+    playerActive.className = 'active';
+    activePlayer = 0;
+
+});
+
+function switchPlayers(){
+    var playerActive = document.querySelector('.player-'+ activePlayer +'-panel');
+    playerActive.className = playerActive.className.replace("active", "");
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    var playerActive2 = document.querySelector('.player-'+ activePlayer +'-panel');
+    playerActive2.className = playerActive2.className += ' active';
+}
+
+
+
+
